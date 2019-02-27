8ada4fac4443fe07828f1a052114b9bf2e60ff53
AdolfoRivera <36741916+AdolfoRivera@users.noreply.github.com>
2019-02-27 09:06:10 -0600

    se modifico a php la pestaña nosotros

--- Nosotros.php
+++ Nosotros.php
@@ -1,11 +1,15 @@
-<!DOCTYPE html>
+<!--<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>canvas</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     
-     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700i" rel="stylesheet">
+     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700i" rel="stylesheet">-->
+<?php
+include 'header.php';
+?>
+
     <style>
     
      * {
@@ -175,8 +179,8 @@ body {
         }
     
     </style>
-</head>
-<body>
+<!--</head>
+<body>-->
     <div class="page">
   <span class="menu_toggle">
     <i class="menu_open fa fa-bars fa-lg"></i>
@@ -224,5 +228,9 @@ $('.content').on('click', function(){
 });
     
 </script>
-</body>
-</html>
+<!--</body>-->
+<!--</html>-->
+<?php
+include 'footer.php';
+?>
+
--- header.php
+++ header.php
@@ -1,6 +1,14 @@
 <!doctype HTML>
 <html lang="es">
-<head>  
+<head>
+  
+
+  <!--ESTILOS PARA LA PESTAÑA NOSOTROS-->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700i" rel="stylesheet">
+  <!--FIN DE ESTILOS PARA LA PESTAÑA NOSOTROS-->
+
+
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
