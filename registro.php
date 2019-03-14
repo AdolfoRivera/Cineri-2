8622d31909dadbe059e9eb63f51d5dcb774aa920
irvincore <matt.9405@gmail.com>
2019-03-14 17:12:14 -0600

    Creacion de la bd corranlo en su SGBD

--- registro.php
+++ registro.php
@@ -4,7 +4,7 @@ include "header.php";
 <div class="foro">
 <form onSubmit="return validar(this)" action="registrar.php" name="reg" method="post" class="formulario">
         <input type="text" placeholder="Nombre">
-        <input type="text" placeholder="Apellido ">
+        <input type="text" placeholder="Apellidos ">
         <input type="email" placeholder="Correo Electronico">
         <input type="password" placeholder="Contraseña">
         <button class="entrar" type="submit" name="iniciar" class="btn">Registrar</button>
