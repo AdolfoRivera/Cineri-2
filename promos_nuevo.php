dd34294471b2d943bc303a9753957036799d111f
Santos de  Jesus Ocejo Flores <santosocejflores@gmail.com>
2019-03-25 08:40:07 -0600

    Modificacion Santos

--- promos_nuevo.php
+++ promos_nuevo.php
@@ -4,44 +4,26 @@ include "header.php";
 ?>
 
 <!-- SECCION PROYECTOS -->
-<br>
-<br>
-<br>
-
-<div class="col-sm-1 ">
-<section class="Promocion ">
-  <div class="Promo-prin">
+<div class="container">
     <h1 class="titulo_promocion">Promociones</h1>
-    <div class="promo-prin2">
-    <article id="promo_art1" class="promo_art">
-        <a class="promo" href="#">
+    
+    <div class="row">
+        <a class="col-3" href="#">
         <img src="images_promo/promocion1.jpg" href="¡Gana con las estuatillas cinemex!">
         </a>
-     </article>
      
-     <article id="promo_art2" class="promo_art">
-      <a class="promo_a" href="#">
+      <a class="col-3 href="#">
           <img src="images_promo/promocion2.jpg "href="¡La mejor promo para amigo!">
-      </a>   
-     </article>
-
-     <article id="promo_art3" class="promo_art">
-         <a class="promo_a" href="#">
+    
+         <a class="col-3" href="#">
             <img src="images_promo/promocion3.jpg" href="¡Regresa Tickatizate!">
          </a>
-     </article>
-     
-     <article id="promo_art4" class="promo_art">
-            <a class="promo_a" href="#">
+      
+            <a class="col-3" href="#">
             <img src="images_promo/promocion4.jpg" href="¡Aprovecha cinefan!">
             </a>
-    </article>
+     </div>
     </div>
-  </div>
-
-
-</section>
-</div>
 <?php
 include "footer.php";
 ?>
