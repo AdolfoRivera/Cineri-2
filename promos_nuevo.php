8edc4b71b17602a59616be9107fd8db6c5b19d76
Santos de  Jesus Ocejo Flores <santosocejflores@gmail.com>
2019-04-09 21:57:03 -0500

    Columna vacia offset-1

--- promos_nuevo.php
+++ promos_nuevo.php
@@ -4,27 +4,165 @@ include "header.php";
 ?>
 
 <!-- SECCION PROYECTOS -->
+<!--<div class="container"> 
+<div class="row" >
+<div class="pro">
+<div class="content-all">
+<a class="col-lg-3 col-md-3 col-sm-12">
+	<div class="content-img">
+        <img src="images_promo/promocion1.jpg" width="100%" height="100%" >
+
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+    </div>
+</a>
+  
+
+ <a class="col-lg-3 col-md-6 col-sm-12">
+    <div class="content-img">
+	
+		<img src="images_promo/promocion2.jpg" width="100%" height="100%">
+
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+
+   </div>
+</a>
+
+
+<a class="col-lg-3 col-md-6 col-sm-12">
+   <div class="content-img">
+	
+		<img src="images_promo/promocion3.jpg" width="100%" height="100%">
+
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+	
+
+   </div>
+</a>
+
+   
+ <a class="col-lg-3 col-md-6 col-sm-12">
+    <div class="content-img">
+	
+		<img src="images_promo/promocion4.jpg" width="100%" height="100%">
+
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+   </div>
+</a>
+
+
+</div>
+</div>
+</div>
+</div>
+-->
+<!-- SECCION PROYECTOS -->9
 <div class="container">
     <h1 class="titulo_promocion">Promociones</h1>
     
     <div class="row">
-    <div class="pro">
-        <a class="col-lg-3 col-md-6 col-sm-12"href="#">
-        <img src="images_promo/promocion1.jpg" ref="¡Gana con las estuatillas cinemex!">
-        </a>
-      <a class="col-lg-3 col-md-6 col-sm-12" href="#">
-          <img src="images_promo/promocion2.jpg "href="¡La mejor promo para amigo!">
-    
-         <a class="col-lg-3 col-md-6 col-sm-12 " href="#">
-            <img src="images_promo/promocion3.jpg" href="¡Regresa Tickatizate!">
-         </a>
-      
-            <a class="col-lg-3 col-md-6 col-sm-12" href="#">
-            <img src="images_promo/promocion4.jpg" href="¡Aprovecha cinefan!">
-            </a>
-     </div>
-    </div>
+        <div class="col-lg-2 col-md-5 col-sm-11 offset-1"href="#">
+        <div class="content-img">
+	
+		<img src="images_promo/promocion1.jpg">
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+
 </div>
+</div>
+      <div class="col-lg-2 col-md-5 col-sm-11 offset-1" href="#">
+      <div class="content-img">
+	
+    <img src="images_promo/promocion2.jpg">
+
+    <div class="content-txt">
+        <h2>Coleccion de Lego</h2>
+        <p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+    </div>
+
+    <div class="content-1"></div>
+    <div class="content-2"></div>
+    <div class="content-3"></div>
+    <div class="content-4"></div>
+
+</div>
+</div>
+         <div class="col-lg-2 col-md-5 col-sm-11 offset-1" href="#">
+         <div class="content-img">
+	
+    <img src="images_promo/promocion3.jpg">
+
+    <div class="content-txt">
+        <h2>Coleccion de Lego</h2>
+        <p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+    </div>
+
+    <div class="content-1"></div>
+    <div class="content-2"></div>
+    <div class="content-3"></div>
+    <div class="content-4"></div>
+
+
+</div>
+</div>
+      
+            <div class="col-lg-2 col-md-5 col-sm-11 offset-1" href="#">
+            <div class="content-img">
+	
+		<img src="images_promo/promocion4.jpg">
+
+		<div class="content-txt">
+			<h2>Coleccion de Lego</h2>
+			<p>Con la coleccion de lego puedes ganar una figura coleccionable de lego, no te lo pierdas, solo por tiempo limitado</p>
+		</div>
+
+		<div class="content-1"></div>
+		<div class="content-2"></div>
+		<div class="content-3"></div>
+		<div class="content-4"></div>
+</div>
+</div>
+</div>
+</div>
+
 <?php
 include "footer.php";
 ?>
